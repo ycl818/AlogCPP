30b35a02c7da1e67a359e861e90c990acbbb241a
yichien <kevinyichienlee@gmail.com>
2022-07-06 23:51:28 +0800

    focus on partial adjust question

---
+++ CF1609B.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int n, q;
+string s;
+void slove() {
+  int now = 0;
+  for (int i = 0; i + 3 <= n; i++) {
+    now += (s.substr(i,3) == "abc");
+  }
+  while(q--) {
+    int pos;
+    char c;
+    cin >> pos >> c;
+    pos--;
+    for (int i = max(0, pos-2); i <= min(pos, n-2); i++) {
+      now -= (s.substr(i, 3) == "abc");
+    }
+    s[pos] = c;
+    for (int i = max(0, pos-2); i <= min(pos, n-2); i++) {
+      now += (s.substr(i, 3) == "abc");
+    }
+    cout << now << endl;
+  }
+}
+
+int main() {
+  cin >> n >> q >> s;
+  slove();
+  return 0;
+}
